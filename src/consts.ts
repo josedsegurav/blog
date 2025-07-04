eaec25393697988b510261dbbbd2fc9861bd3792
Jose Segura <josed.segurav@gmail.com>
2025-07-04 14:36:25 -0500

    Updated site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ConsoleLog Life';
-export const SITE_DESCRIPTION = 'Everyday struggles without a name. But fun, we have to be positive right';
+export const SITE_TITLE = "ConsoleLog Life";
+export const SITE_DESCRIPTION =
+  "Welcome to my personal blog! A space where I share my journey—the ups, downs, lessons, experiences, and plenty of struggles.";
